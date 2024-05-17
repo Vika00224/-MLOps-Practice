7fd80b6121c64511e2daa0a09685074da82de967
Vika Ilinykh <i-vika00@mail.ru>
2024-05-17 19:31:43 +0500

    add file

--- lab2/data_creation.py
+++ lab2/data_creation.py
@@ -1,15 +1,23 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import os
+from sklearn.datasets import load_wine
 
-df = pd.read_csv('datasets/Thyroid_Diff.csv')
-df['Recurred'].replace(['Yes', 'No'], ['1', '2'])
-#df['Recurred'].replace('No', '0')
-df.to_csv('datasets/NEW.csv', index=False)
-# разбиваем на тренировочную и валидационную
-x_train, x_val, = train_test_split(df, test_size=0.2,random_state=42)
-# сохранение данных в csv
-x_train.to_csv('train.csv', index=False)
-x_val.to_csv('test.csv', index=False)
+# загружаем датасет
+wine = load_wine()
+X = wine.data  # type: ignore
+y = wine.target  # type: ignore
 
-print('Tренировочные данные: сохранены')
-print('Тестовые данные: сохранены')
+# трансформируем в датафрейм
+df = pd.DataFrame(data=X, columns=wine.feature_names)  # type: ignore
+df['target'] = y
+
+print(df.info())
+print(df.describe())
+
+# создание каталогов для хранения данных
+os.makedirs('datasets', exist_ok=True)
+
+# сохранение CSV файла
+df.to_csv('datasets/wine.csv', index=False)
+
+print('Данные сохранены')
