ec742000ec2c4ec433afbf6399938f7e5de368d1
Vika Ilinykh <i-vika00@mail.ru>
2024-05-17 18:03:01 +0500

    add file

--- lab2/data_creation.py
+++ lab2/data_creation.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('datasets/Thyroid_Diff.csv')
+df['Recurred'].replace(['Yes', 'No'], ['1', '2'])
+#df['Recurred'].replace('No', '0')
+df.to_csv('datasets/NEW.csv', index=False)
 # разбиваем на тренировочную и валидационную
 x_train, x_val, = train_test_split(df, test_size=0.2,random_state=42)
 # сохранение данных в csv
